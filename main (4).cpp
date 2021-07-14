6382efaac9554ece0996f709ade717a245c2f776
Devprasanna1905 <70941315+Devprasanna1905@users.noreply.github.com>
2021-07-14 10:14:52 +0530

    Add files via upload

---
+++ main (4).cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>  
+using namespace std;  
+int main()  
+{  
+  int n, i, flag=0;  
+  cout << "Enter the Number to check Prime: ";  
+  cin >> n;  
+    
+  for(i = 1; i <= n; i++)  
+  {  
+      if(n % i == 0)  
+      {  
+          flag++;  
+           
+      }  
+  }  
+  if (flag==2)  
+      cout << "Number is Prime."<<endl;  
+  return 0;  
+}  
